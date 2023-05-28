2f36d9ff9a3902e2a32a9125920b07c4d23f28f0
JMY1993 <yangmohan1993@163.com>
2023-05-28 20:01:39 +0800

    count words

---
+++ chapter1/countWords.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main(){
+
+    int inside = OUT;
+
+    int nl, nw, nc;
+    nl = nw = nc = 0;
+
+    while (getchar() != EOF) {
+        
+    }
+}
