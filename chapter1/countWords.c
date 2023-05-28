55d788e5c4ff25a8b459232cfac59a8da6e35c9e
JMY1993 <yangmohan1993@163.com>
2023-05-28 23:21:05 +0800

    countWords

--- chapter1/countWords.c
+++ chapter1/countWords.c
@@ -1,3 +1,7 @@
+/* count word version 1.0 
+* this is just a bare-bone version of linux program wc
+*/
+
 #include <stdio.h>
 
 #define IN 1
@@ -10,7 +14,19 @@ int main(){
     int nl, nw, nc;
     nl = nw = nc = 0;
 
-    while (getchar() != EOF) {
-        
+    int chr;
+
+    while ((chr = getchar()) != EOF) {
+        ++nc;
+        if (chr == '\n') ++nl;
+
+        if (chr == '\t' || chr == '\n' || chr == ' ') {
+            inside = OUT;
+        } else if (inside == OUT) {
+            inside = IN;
+            ++nw;
+        }
     }
+
+    printf("\t %d \t %d \t %d\n", nl, nw, nc);
 }
